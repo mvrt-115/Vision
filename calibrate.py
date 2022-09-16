607c1219180befb7b92859bd62f987fea47ea6a5
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-09-15 21:50:09 -0700

    Got a camera matrix?

--- calibrate.py
+++ calibrate.py
@@ -21,15 +21,35 @@ images = glob.glob('calibrate.jpg')
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
+
         # Draw and display the corners
         cv.drawChessboardCorners(img, (7,6), corners2, ret)
         cv.imshow('img', img)
-        cv.waitKey(0)
-cv.destroyAllWindows()
+
+#calibrate
+#camera matrix, distortion coefficients, rotation and translation vectors
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+print (mtx)
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+cv.waitKey(0)
