We were using wrong index for angle lol, should be good now

src/localize.py
@@ -4,6 +4,11 @@ import cv2
 import time
 import math
 
+import sys
+
+sys.path.insert(0, 'utils/')
+from jsontools import JsonTools
+
 detector = Detector(families='tag36h11',
                     nthreads=1,
                     quad_decimate=1.0,
@@ -42,10 +47,12 @@ def draw_detect(frame):
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     tags = detector.detect(grayImage, estimate_tag_pose=True, camera_params=(3156.71852, 3129.52243, 359.097908, 239.736909), tag_size=.038)
 
-    fx =  236.77719488200282
-    fy =  240.2921754424293
-    cx =  422.68668295039623
-    cy =  82.50299878265058
+    #Read focal length and camera stuff from file
+    tools = JsonTools()
+    fx = tools.getJsonVal("files/matrix.txt", "fx")
+    fy = tools.getJsonVal("files/matrix.txt", "fy")
+    cx = tools.getJsonVal("files/matrix.txt", "cx")
+    cy = tools.getJsonVal("files/matrix.txt", "cy")
 
     tags = detector.detect(grayImage, estimate_tag_pose=True, camera_params=[fx, fy, cx, cy], tag_size=0.161)
 
@@ -70,8 +77,8 @@ def draw_detect(frame):
 #Print out coords and angle relative to field
 def printall(t):
     #Coordinates relative to field
-    cords = get_coords(t)
-    print(f'Robot  Pos: x:{cords[0]} y:{cords[1]}')
+    #cords = get_coords(t)
+    #print(f'Robot  Pos: x:{cords[0]*10} y:{cords[1]*10}')
 
     #Angle relative to field
     angle = get_angle(t)
@@ -89,6 +96,7 @@ def get_coords(t):
     theta = 3.1415926/2
     dx = -math.sqrt(x**2+y**2)*math.cos(theta+math.atan(x/y))
     dy = math.sqrt(x**2+y**2)*math.sin(theta+math.atan(x/y))
+
     # Calculating ROBOT coordinates
     coordX = aprilX +dx
     coordY = aprilY + dy
@@ -105,7 +113,7 @@ def rot2eul(R):
     beta = -np.arcsin(R[2,0])
     alpha = np.arctan2(R[2,1]/np.cos(beta),R[2,2]/np.cos(beta))
     gamma = np.arctan2(R[1,0]/np.cos(beta),R[0,0]/np.cos(beta))
-    a = np.array((alpha, beta, gamma))[0]
+    a = np.array((alpha, beta, gamma))[1]
     return a
 
 main()